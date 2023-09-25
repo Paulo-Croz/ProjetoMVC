9d72cb16056ee63b65ef3e6bb9b4f12b6156554f
Paulo Sergio <paulo1999.sergio.lopes@gmail.com>
2023-09-25 20:11:32 -0300

    CRUD Scaffolding

--- ProjetoMVC/Startup.cs
+++ ProjetoMVC/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Data;
 
 namespace ProjetoMVC
 {
@@ -24,6 +26,9 @@ namespace ProjetoMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProjetoMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
