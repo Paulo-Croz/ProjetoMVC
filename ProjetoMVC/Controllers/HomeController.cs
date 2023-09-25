30332e7c8b6874b65e1e59142d2ae68b479cdb9c
Paulo Sergio <paulo1999.sergio.lopes@gmail.com>
2023-09-25 17:04:26 -0300

    Modificando texto no html

--- ProjetoMVC/Controllers/HomeController.cs
+++ ProjetoMVC/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace ProjetoMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Pagina com projeto MVC.";
 
             return View();
         }
