86875d929d62a78d833139040abffb6c59c3ed0f
Paulo Sergio <paulo1999.sergio.lopes@gmail.com>
2023-09-26 15:10:47 -0300

    Detalhes vendedor e carregamento ansioso

--- ProjetoMVC/Services/SellerService.cs
+++ ProjetoMVC/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using ProjetoMVC.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoMVC.Services
 {
@@ -25,7 +26,7 @@ namespace ProjetoMVC.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
