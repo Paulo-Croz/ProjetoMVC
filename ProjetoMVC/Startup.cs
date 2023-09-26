80ed80970e633b52cc6cacf4faea39821883eedf
Paulo Sergio <paulo1999.sergio.lopes@gmail.com>
2023-09-26 00:07:34 -0300

    Seeding Service

--- ProjetoMVC/Startup.cs
+++ ProjetoMVC/Startup.cs
@@ -30,14 +30,16 @@ namespace ProjetoMVC
             services.AddDbContext<ProjetoMVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("ProjetoMVCContext"), builder => 
                         builder.MigrationsAssembly("ProjetoMVC")));
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
