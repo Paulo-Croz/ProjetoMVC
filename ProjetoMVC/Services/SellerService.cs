ceb6592b4ea507700d94910be5a204775fa8409c
Paulo Sergio <paulo1999.sergio.lopes@gmail.com>
2023-09-29 16:37:33 -0300

    Atualizacao vendedor e customizacao da execao do servico

--- ProjetoMVC/Services/SellerService.cs
+++ ProjetoMVC/Services/SellerService.cs
@@ -35,5 +35,22 @@ namespace ProjetoMVC.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new KeyNotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbUpdateConcurrencyException(e.Message);
+            }
+        }
     }
 }
